08cce337d4e7b361935b345ad3afe49ee1ce7c7a
kimyen891 <nguyenthikimyen891@gmail.com>
2024-12-06 17:09:32 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/chinese-learning",
-    output: "export",
+    //output: "export",
     images: {
         remotePatterns: [
             {
